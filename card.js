dde10b2d0edb615d6c47659da63bf8a0d235f16a
MaiSang <sarusasiri@gmail.com>
2025-02-18 12:11:06 +0700

    Update card.js

--- card.js
+++ card.js
@@ -1,24 +1,22 @@
 let cardSelect = [];
-document.addEventListener("DOMContentLoaded", function () {
-  let cardsData = []; // เก็บข้อมูลจาก JSON
 
-  // โหลดข้อมูลจาก JSON
-  fetch("../cards.json")
-    .then((response) => response.json())
-    .then((data) => {
-      cardsData = data; // บันทึกข้อมูลลงตัวแปร
+const loadCards = async () => {
+  let cardsData = [];
+  try {
+    const response = await fetch("./cards.json"); // โหลด JSON
+    if (!response.ok) throw new Error("โหลด JSON ไม่ได้");
 
-      console.log(cardsData);
+    cardsData = await response.json();
+    console.log("โหลดข้อมูลสำเร็จ:", cardsData);
+    if (cardsData.length === 0) return null; // เช็คว่ามีข้อมูลไหม
 
-      const displayRandomCard = () => {
-        if (cardsData.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * cardsData.length);
+    cardSelect = cardsData[randomIndex];
+    console.log(cardSelect);
+  } catch (error) {
+    console.error(" Error loading JSON:", error);
+  }
+};
 
-        const randomIndex = Math.floor(Math.random() * cardsData.length);
-        return cardsData[randomIndex];
-      };
-      cardSelect = displayRandomCard();
-      console.log(cardSelect);
-    })
-    .catch((error) => console.error("Error loading JSON:", error));
-});
+loadCards();
 export { cardSelect };
