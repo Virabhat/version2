f84fcabbad56a8beb14838c02f7b057b714e01a0
MaiSang <sarusasiri@gmail.com>
2025-02-19 18:04:12 +0700

    Add :เพิ่มปุ่มกดดูความหมาย

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 import { cardSelect } from "./card.js";
 
+function LoadOnetime() {
+  if (sessionStorage.getItem("isLoad")) return; // ถ้าเคยโหลดแล้ว ให้ออกจากฟังก์ชัน
+
+  sessionStorage.setItem("isLoad", "true"); // บันทึกว่าโหลดแล้ว
+  window.location.href =
+    window.location.origin +
+    window.location.pathname +
+    "?nocache=" +
+    new Date().getTime();
+}
+
+LoadOnetime();
+
 var wheel = $("#wheel");
 let isWheelSpun = false;
 var border = parseInt(wheel.css("border-width"));
@@ -42,6 +55,7 @@ const draggableWheel = Draggable.create(wheel, {
     console.log(this);
   },
   liveSnap: liveSnap,
+
   dragResistance: 0.3, //slow rotate
   throwResistance: 0.6,
 });
@@ -151,7 +165,7 @@ const selectCard = document.querySelectorAll(".image-box");
 const frontCard = document.querySelector(".front-card");
 const meaning = document.querySelector(".meaning");
 const cardName = document.querySelector(".name_card");
-
+const btn = document.querySelector(".btn-meaning");
 let isCardClicked = false;
 // selectCard[1].addEventListener("click", (e) => {
 //   e.preventDefault();
@@ -193,103 +207,144 @@ selectCard.forEach((card, index) => {
         },
       })
 
+      .set(wheel, { display: "none" })
       .set(card, { yPercent: 0 })
       .set(active, { opacity: 0, display: "grid" })
 
       //เฟดและ ขยายdiv active
       .to(active, { opacity: 1, duration: 1, ease: "power4.out" })
       .to(active, { duration: 0.7, scale: 1.2, ease: "power4.out" })
-
-      //พลิกจากหลังไปหน้า
       .to(card, {
-        delay: 0.3,
+        scale: 1.1,
+        duration: 0.7,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+    // คลิกcard แล้วเล่นอนิมเชั่นถัดไป
+    card.addEventListener("click", (e) => {
+      e.preventDefault();
+      const currentTweens = gsap.getTweensOf(card);
+
+      // ถ้ามีอนิเมชั่นทำงานอยู่ให้หยุดมันก่อน
+      if (currentTweens.length > 0) {
+        currentTweens.forEach((tween) => {
+          tween.kill(); // หยุดอนิเมชั่น
+        });
+      }
+
+      const tl = gsap.timeline();
+      tl.to(card, {
         duration: 0.5,
-        rotationY: 90,
-        ease: "expo.inOut",
+        scale: 1,
+        ease: "sine.inOut",
       })
+        //พลิกจากหลังไปหน้า
+        .to(card, {
+          duration: 0.5,
+          rotationY: 90,
+          ease: "expo.inOut",
+        })
 
-      .set(card, { display: "none" })
-      .set(frontCard, { display: "flex" })
-      .set(meaning, { display: "flex" })
-      .set(cardName, { display: "flex" })
+        .set(card, { display: "none" })
+        .set(frontCard, { display: "flex" })
+        .set(cardName, { display: "flex" })
+        .set(btn, { display: "flex" })
 
-      .to(frontCard, {
-        duration: 0.35,
-        yPercent: 0,
-        rotationY: 0,
-        ease: "back.out(2)",
-      })
+        .to(frontCard, {
+          duration: 0.35,
+          yPercent: 0,
+          rotationY: 0,
+          ease: "back.out(2)",
+        })
 
-      .to(cardName, {
-        opacity: 1,
-        duration: 0.8,
-        ease: "power2.out",
-      })
-      .to(
-        meaning,
-        {
+        .to(cardName, {
           opacity: 1,
           duration: 0.8,
           ease: "power2.out",
-        },
-        "<"
-      );
+        })
+        .to(
+          btn,
+          {
+            opacity: 1,
+            duration: 0.8,
+            ease: "power2.out",
+          },
+          "<"
+        );
 
-    //แบบเก่า
-
-    // activeCard = card;
-    // active.appendChild(card);
-    // gsap.set(active, { opacity: 0, display: "block" });
-
-    // gsap.to(wheel, {
-    //   opacity: 0,
-    //   duration: 1,
-    //   delay: 0.5,
-    //   ease: "power4.out",
-    //   onComplete: () => {
-    //     gsap.to(active, {
-    //       opacity: 1,
-    //       duration: 1,
-    //       ease: "power4.out",
-    //       onStart: () => {
-    //         console.log("เริ่มโชว์ active");
-    //       },
-    //       onComplete: () => {
-    //         console.log("โชว์ active สำเร็จ"); // Debug
-    //         card.classList.add("flip");
-    //         gsap.to(active, {
-    //           duration: 0.7,
-    //           scale: 1.2,
-    //           ease: "power4.out",
-    //         });
-    //       },
-    //     });
-    //   },
-    // });
-    // gsap.to(card, {
-    //   duration: 1,
-    //   yPercent: 0,
-    //   bounds: 0,
-    //   scale: 1,
-    //   rotation: 0,
-    //   delay: 3,
-    //   ease: "expo.out",
-    //   onComplete: () => {
-    //     gsap.to(card, {
-    //       duration: 0.5,
-    //       rotationY: 90,
-    //       ease: "power2.inOut",
-    //       onComplete: () => {
-    //         card.style.display = "none";
-    //         frontCard.style.display = "flex";
-    //         gsap.to(frontCard, {
-    //           duration: 0.5,
-    //           rotationY: 0,
-    //           ease: "power2.inOut",
-    //         });
-    //       },
-    //     });
-    //   },
-    // });
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const tl = gsap.timeline();
+        tl.to(btn, {
+          opacity: 0,
+          duration: 0.8,
+          ease: "power2.out",
+          onComplete: () => {
+            gsap.set(meaning, { display: "flex" });
+          },
+        }).to(meaning, {
+          opacity: 1,
+          duration: 0.8,
+          ease: "power2.out",
+        });
+      });
+    });
   });
+  //แบบเก่า
+
+  // activeCard = card;
+  // active.appendChild(card);
+  // gsap.set(active, { opacity: 0, display: "block" });
+
+  // gsap.to(wheel, {
+  //   opacity: 0,
+  //   duration: 1,
+  //   delay: 0.5,
+  //   ease: "power4.out",
+  //   onComplete: () => {
+  //     gsap.to(active, {
+  //       opacity: 1,
+  //       duration: 1,
+  //       ease: "power4.out",
+  //       onStart: () => {
+  //         console.log("เริ่มโชว์ active");
+  //       },
+  //       onComplete: () => {
+  //         console.log("โชว์ active สำเร็จ"); // Debug
+  //         card.classList.add("flip");
+  //         gsap.to(active, {
+  //           duration: 0.7,
+  //           scale: 1.2,
+  //           ease: "power4.out",
+  //         });
+  //       },
+  //     });
+  //   },
+  // });
+  // gsap.to(card, {
+  //   duration: 1,
+  //   yPercent: 0,
+  //   bounds: 0,
+  //   scale: 1,
+  //   rotation: 0,
+  //   delay: 3,
+  //   ease: "expo.out",
+  //   onComplete: () => {
+  //     gsap.to(card, {
+  //       duration: 0.5,
+  //       rotationY: 90,
+  //       ease: "power2.inOut",
+  //       onComplete: () => {
+  //         card.style.display = "none";
+  //         frontCard.style.display = "flex";
+  //         gsap.to(frontCard, {
+  //           duration: 0.5,
+  //           rotationY: 0,
+  //           ease: "power2.inOut",
+  //         });
+  //       },
+  //     });
+  //   },
+  // });
 });
