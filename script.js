1ebfdedb07d64d3a5cb9b0506e443c26fa37a1b9
MaiSang <sarusasiri@gmail.com>
2025-02-24 15:50:31 +0700

    Edit : fix ios select wrong card

--- script.js
+++ script.js
@@ -73,11 +73,18 @@ function createBox(i) {
     .appendTo(wheel);
 
   // var internalBox = $("<div class='internalBox' />").text(num).appendTo(box);
-
+  var placeholder = $(
+    "<div class='image-placeholder'>Loading...</div>"
+  ).appendTo(box);
   //img
   var image = $(
-    "<img class='image-box' src='./All_Pic/Tarot card/Card.png' alt='Card Image' />"
+    "<img class='image-box' src='./All_Pic/Tarot card/Card.png' alt='Card Image' loading='lazy'/>"
   ).appendTo(box);
+
+  image.on("load", function () {
+    placeholder.hide();
+    $(this).fadeIn();
+  });
 
   TweenLite.set(box, {
     rotation: angle + "_rad",
@@ -148,19 +155,15 @@ const cardName = document.querySelector(".name_card");
 const btn = document.querySelector(".btn-meaning");
 const containerInfo = document.querySelector(".container-info ");
 let isCardClicked = false;
-// selectCard[1].addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log("index");
-// });
-// console.log(selectCard);
 
 cards.forEach((selectCard, index) => {
   selectCard.addEventListener("click", (e) => {
     e.preventDefault();
-    // if (!isWheelSpun) return;
     if (isCardClicked) return;
 
     console.log("Card clicked:", index);
+    // const f = document.querySelector(".test");
+    // f.innerHTML = "Select :" + index;
     draggableWheel[0].disable(); //ปิดการหมุน
     isCardClicked = true;
     selectCard.style.cursor = "default";
@@ -184,6 +187,7 @@ cards.forEach((selectCard, index) => {
         ease: "power4.out",
         onComplete: () => {
           cardContainer.appendChild(selectCard);
+          wheel.remove();
         },
       })
 
@@ -201,86 +205,96 @@ cards.forEach((selectCard, index) => {
         yoyo: true,
         ease: "sine.inOut",
       });
+
     // คลิกcard แล้วเล่นอนิมเชั่นถัดไป
-    selectCard.addEventListener("pointerup", (e) => {
-      e.preventDefault();
-      const currentTweens = gsap.getTweensOf(selectCard);
+    selectCard.addEventListener(
+      "pointerup",
+      (e) => {
+        e.preventDefault();
+        const currentTweens = gsap.getTweensOf(selectCard);
 
-      // ถ้ามีอนิเมชั่นทำงานอยู่ให้หยุดมันก่อน
-      if (currentTweens.length > 0) {
-        currentTweens.forEach((tween) => {
-          tween.kill(); // หยุดอนิเมชั่น
-        });
-      }
-      gsap.to(cardContainer, {
-        scale: 1,
-        duration: 1,
-        ease: "power2.out",
-        // transform: "translateY(-10%)",
-      });
-
-      const tl = gsap.timeline();
-
-      tl.to(containerInfo, {
-        opacity: 1,
-        duration: 0.5,
-        ease: "power2.out",
-      })
-        .to(selectCard, {
-          duration: 0.5,
+        // ถ้ามีอนิเมชั่นทำงานอยู่ให้หยุดมันก่อน
+        if (currentTweens.length > 0) {
+          currentTweens.forEach((tween) => {
+            tween.kill(); // หยุดอนิเมชั่น
+          });
+        }
+        gsap.to(cardContainer, {
           scale: 1,
-          ease: "sine.inOut",
-        })
-        //พลิกจากหลังไปหน้า
-        .to(selectCard, {
-          duration: 0.5,
-          rotationY: 90,
-          ease: "expo.inOut",
-        })
+          duration: 1,
+          ease: "power2.out",
+          // transform: "translateY(-10%)",
+        });
 
-        .set(selectCard, { display: "none" })
-        .set(frontCard, { display: "flex" })
-        .set(cardName, { display: "flex" })
-        .set(btn, { display: "flex" })
+        const tl = gsap.timeline();
 
-        .to(frontCard, {
-          duration: 0.35,
-          yPercent: 0,
-          rotationY: 0,
-          ease: "back.out(2)",
-        })
-
-        .to(cardName, {
+        tl.to(containerInfo, {
           opacity: 1,
-          duration: 0.8,
+          duration: 0.5,
           ease: "power2.out",
         })
-        .to(
-          btn,
-          {
+          .to(selectCard, {
+            duration: 0.5,
+            scale: 1,
+            ease: "sine.inOut",
+          })
+          //พลิกจากหลังไปหน้า
+          .to(selectCard, {
+            duration: 0.4,
+            rotationY: 90,
+            ease: "expo.inOut",
+          })
+
+          .set(selectCard, { display: "none" })
+          .set(frontCard, { display: "flex" })
+          .set(cardName, { display: "flex" })
+          .set(btn, { display: "flex" })
+
+          .to(frontCard, {
+            duration: 0.35,
+            yPercent: 0,
+            rotationY: 0,
+            ease: "back.out(2)",
+            onComplete: () => {
+              gsap
+                .timeline()
+                .to(cardName, {
+                  opacity: 1,
+                  duration: 0.8,
+                  ease: "power2.out",
+                })
+                .to(
+                  btn,
+                  {
+                    opacity: 1,
+                    duration: 0.8,
+                    ease: "power2.out",
+                    pointerEvents: "auto",
+                  },
+                  "<"
+                );
+            },
+          });
+
+        btn.addEventListener("click", (e) => {
+          e.preventDefault();
+          const tl = gsap.timeline();
+          tl.to(btn, {
+            opacity: 0,
+            duration: 0.8,
+            ease: "power2.out",
+            onComplete: () => {
+              gsap.set(meaning, { display: "flex" });
+              gsap.set(btn, { display: "none" });
+            },
+          }).to(meaning, {
             opacity: 1,
             duration: 0.8,
             ease: "power2.out",
-          },
-          "<"
-        );
-
-      btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        const tl = gsap.timeline();
-        tl.to(btn, {
-          opacity: 0,
-          duration: 0.8,
-          ease: "power2.out",
-          onComplete: () => {
-            gsap.set(meaning, { display: "flex" });
-          },
-        }).to(meaning, {
-          opacity: 1,
-          duration: 0.8,
-          ease: "power2.out",
+          });
         });
-      });
-    });
+      },
+      { once: true }
+    );
   });
 });
